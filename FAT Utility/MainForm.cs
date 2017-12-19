27d2d8ce9eb0f7dca9ec28f92b466d1b6cbf6f5f
Brian W <masta_bla@hotmail.com>
2017-12-19 00:17:00 -0600

    .

--- FAT Utility/MainForm.cs
+++ FAT Utility/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FAT_Utility
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
--- FAT Utility/Program.cs
+++ FAT Utility/Program.cs
@@ -16,7 +16,7 @@ namespace FAT_Utility
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
